1f2551528cdcefc7b09c077f7ab9a928a5f81d33
AlvarDVK <alvarc98@gmail.com>
2021-01-17 18:25:00 +0100

    añadido el mínimo

--- Funcion que devuelve función.py
+++ Funcion que devuelve función.py
@@ -7,4 +7,14 @@ def maxi(*l):
             n = l[ix]
     return n
 
+def mini(*l):
+    if len(l) == 0:
+        return 0
+    n = l[0]
+    for ix in range(1, len(l)):
+        if l[ix] > n:
+            n = l[ix]
+    return n
+
 print(maxi(1, 3, 8, 5, 6))
+print(mini(1, 3, 8, 5, 6))
