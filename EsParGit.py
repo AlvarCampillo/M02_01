66f731c36aad7353a5082b4d6763b9b7a48d5a45
AlvarDVK <alvarc98@gmail.com>
2021-01-17 17:40:12 +0100

    Mejora de la función

--- EsParGit.py
+++ EsParGit.py
@@ -1,5 +1,11 @@
 def esPar(n):
     if n % 2 == 0:
-        print("Es par")
+        return True
     else:
-        print("No es par")
+        return False
+
+num = int(input("Introduce un número"))
+if esPar(num):
+    print("El número {} es par".format(num))
+else:
+    print("El número {} no es par".format(num))
