a9a0250e6915425f4bbd300d334e750eeb340b8f
AlvarDVK <alvarc98@gmail.com>
2021-01-17 18:22:53 +0100

    Función que devuelve función: máximo

---
+++ Funcion que devuelve función.py
@@ -0,0 +1,10 @@
+def maxi(*l):
+    if len(l) == 0:
+        return 0
+    n = l[0]
+    for ix in range(1, len(l)):
+        if l[ix] > n:
+            n = l[ix]
+    return n
+
+print(maxi(1, 3, 8, 5, 6))
