a80700a37a7579583911ab821b92c71f5eec1669
AlvarDVK <alvarc98@gmail.com>
2021-01-17 18:33:31 +0100

    Función que devuelve función creada y corregido función min

--- Funcion que devuelve función.py
+++ Funcion que devuelve función.py
@@ -12,9 +12,19 @@ def mini(*l):
         return 0
     n = l[0]
     for ix in range(1, len(l)):
-        if l[ix] > n:
+        if l[ix] < n:
             n = l[ix]
     return n
 
-print(maxi(1, 3, 8, 5, 6))
-print(mini(1, 3, 8, 5, 6))
+funciones = {"min": mini,
+             "max": maxi}
+
+def returnF(nombre):
+    nombre = nombre.lower()
+    if nombre in funciones.keys():
+        return funciones[nombre]
+    
+    return None
+
+print(returnF("max")(1, 3, 8, 5, 6))
+print(returnF("min")(1, 3, 8, 5, 6))
